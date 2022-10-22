e1c99a0d02ef7015be0f706ccdb9318813ede6cf
Tourbut <42790398+tourbut@users.noreply.github.com>
2022-10-22 11:25:10 +0900

    dataset

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import torch
 
 if __name__ == "__main__":
     print('hello world')
+
+    device = torch.device('mps')
+    print(f" - device : {device}")
+    sample = torch.Tensor([[10, 20, 30], [30, 20, 10]])
+    print(f" - cpu tensor : ")
+    print(sample)
+    sample = sample.to(device)
+    print(f" - gpu tensor : ")
+    print(sample)
+
+    
