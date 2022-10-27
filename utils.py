a01fc9eae805d511690824f08a476eee22b39908
Tourbut <42790398+tourbut@users.noreply.github.com>
2022-10-27 15:22:18 +0900

    notebook 생성

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import matplotlib.pyplot as plt
 
 
 def load_txt(txt_dir, txt_name):
@@ -22,3 +23,16 @@ def read_csv(filename):
     labels = [0 if a[1]=='CN' else 1 for a in your_list[1:]]
     return filenames, labels
 
+def load_config():
+    config = read_json('./config.json')
+    return config
+
+def stack_plot(stack,rows=6,cols=6,start_with=10,show_every=5,subtitle='title'):
+    fig,ax = plt.subplots(rows,cols,figsize=[12,12])
+    plt.suptitle(subtitle)
+    for i in range(rows*cols):
+        ind = start_with = i*show_every
+        ax[int(i / rows),int(i % rows)].set_title('slice %d'%ind)
+        ax[int(i / rows),int(i % rows)].imshow(stack[:,:,ind],cmap='gray')
+        ax[int(i / rows),int(i % rows)].axis('off')
+    plt.show()
