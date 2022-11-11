bcc0a88e6615b2fa3e122c281e221cd31b9de593
Tourbut <42790398+tourbut@users.noreply.github.com>
2022-11-11 15:33:39 +0900

    config fix

--- optimizer.py
+++ optimizer.py
@@ -9,7 +9,6 @@ def SGD(model,learning_rate,momentum=0,dampening=0,weight_decay=0):
     return optim.SGD(model.parameters(), lr= learning_rate,momentum=momentum,dampening=dampening,weight_decay=weight_decay)
 
 def adjust_learning_rate(optimizer, epoch,learning_rate, lr_steps=[40, 55, 65, 70, 200, 250]):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr_new = learning_rate * (0.1 ** (sum(epoch >= np.array(lr_steps))))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr_new
